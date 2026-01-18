907cd8a0f7a1e7c9ea2b9ba37a4b0134b7080164
JuanD <pereznjuandiego@gmail.com>
2026-01-17 20:09:04 -0500

    feat: implement KMeans algorithm to store user's encodings

--- apps/server/src/core/users.py
+++ apps/server/src/core/users.py
@@ -1,6 +1,8 @@
 from typing import List
 
-from src.core.constants import IMAGE_EXTENSIONS
+from sklearn.cluster import KMeans
+
+from src.core.constants import IMAGE_EXTENSIONS, K_MEANS_CLUSTERS
 from src.schemas import FaceEncoding
 from src.utils import USERSDIR, face_encodings, load_image_file
 
@@ -26,11 +28,16 @@ def load_users() -> List[FaceEncoding]:
             if photo_encodings:
                 encodings.append(photo_encodings[0])
 
+        # Implement KMeans algorithm to optimize the search for large user bases.
+        if len(encodings) >= K_MEANS_CLUSTERS:
+            kmeans = KMeans(n_clusters=K_MEANS_CLUSTERS, random_state=37)
+            kmeans.fit(encodings)
+            encodings = kmeans.cluster_centers_.tolist()
+
         users.append(FaceEncoding(user=user.name, encodings=encodings))
 
-    # TODO: Implement KNN algorithm to optimize the search for large user bases.
-
     return users
+
 
 # I'll use an in-memory cache for known users to avoid reloading them on each recognition.
 known_users = load_users()
